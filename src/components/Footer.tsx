32942872a7f278bb050e9ef25affbe99431674bd
João Lucas MAIN <joaolucasmmd@gmail.com>
2024-02-22 00:12:47 -0300

    correções

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ export function Footer() {
         <footer className="mt-8 bg-neutral-800 w-full p-5 flex justify-around items-center border-t-[1px] border-neutral-500">
             <p className="font-bebas text-xl tracking-wide">Alunos</p>
             <p>Vitor Galindo de Oliveira</p>
-            <p>João lucas Mota</p>
+            <p>João Lucas Mota</p>
             <p>Lucas Justino</p>
-            <p>Vicotr Callegari</p>
+            <p>Victor Callegari</p>
             <p>Laiane</p>
             <a className="fixed right-4 bottom-4 _default-button p-3" href="#"><ArrowUp size={20}/></a>
         </footer>
