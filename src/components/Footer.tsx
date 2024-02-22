5d6a2258c925a5326860c69aa3c9d3b389c2fe33
João Lucas MAIN <joaolucasmmd@gmail.com>
2024-02-22 00:11:11 -0300

    nomes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,12 @@ import { ArrowUp } from "lucide-react";
 export function Footer() {
     return (
         <footer className="mt-8 bg-neutral-800 w-full p-5 flex justify-around items-center border-t-[1px] border-neutral-500">
-            <p className="font-bebas text-xl tracking-wide">Aula 04</p>
+            <p className="font-bebas text-xl tracking-wide">Alunos</p>
             <p>Vitor Galindo de Oliveira</p>
+            <p>João lucas Mota</p>
+            <p>Lucas Justino</p>
+            <p>Vicotr Callegari</p>
+            <p>Laiane</p>
             <a className="fixed right-4 bottom-4 _default-button p-3" href="#"><ArrowUp size={20}/></a>
         </footer>
     )
